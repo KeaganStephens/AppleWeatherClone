689d0e26069afa09f9ef169b31bda3aa10b1a561
Stephens <Keagan.stephens@icloud.com>
2023-11-02 12:23:35 +0200

    played with flex

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InfoDisplayComponent } from './info-display/info-display.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfoDisplayComponent
+    InfoDisplayComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
